79e636e44f088aed8797435659abfc1d97a1d294
AzureAD\EnricoSucchielli <esucchielli@alpenite.com>
2024-01-22 14:11:22 +0100

    Chapter 2

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page. */
+/* GET chapters page. */
 router.get('/capitolo01', function(req, res, next) {
   res.render('capitolo01', { title: 'Capitolo 01' });
 });
 
+router.get('/capitolo02', function(req, res, next) {
+  res.render('capitolo02', { title: 'Capitolo 02' });
+});
 
 module.exports = router;
