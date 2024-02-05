ec7fc9419603e6af1c1694116d513f3ba4012e52
AzureAD\EnricoSucchielli <esucchielli@alpenite.com>
2024-02-05 14:03:07 +0100

    Added upload

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/chapter');
 
 var app = express();
+
+
+// Configurazione di Multer per il salvataggio dei file
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // La directory dove verranno salvati i file
+  },
+  filename: function (req, file, cb) {
+    // Genera un nome unico per il file
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const fileExtension = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + fileExtension);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +37,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.use('/upload', upload.single('file'));
+
 app.use('/chapter', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET chapters page. */
-router.get('/capitolo01', function(req, res, next) {
-  res.render('capitolo01', { title: 'Capitolo 01' });
-});
-
-router.get('/capitolo02', function(req, res, next) {
-  res.render('capitolo02', { title: 'Capitolo 02' });
-});
-
-router.get('/capitolo03', function(req, res, next) {
-  res.render('capitolo03', { title: 'Capitolo 03' });
-});
-
 module.exports = router;
